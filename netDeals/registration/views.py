05c1fee9fa4031aeb5f395f43b1c416988a5ec14
Kendarius James <94883767+Kendarius-James@users.noreply.github.com>
2023-04-10 16:49:47 -0500

    Removing the old net deals directory
    Updating the net deals files

--- netDeals/registration/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .forms import RegistrationForm
-
-def password_reset_form (request):
-    context = {}
-    context['form'] = RegistrationForm()
-    return render( request, "password_reset_form.html", context)
--- netDeals/seller/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from accounts.models import SellerProfile
-# Register your models here.
-admin.site.register(SellerProfile)

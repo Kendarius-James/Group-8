4aa0c77b78fd4465185f91c832d77443c4dfb63e
Justin Whitfield <91854905+justinwhitfieldd@users.noreply.github.com>
2023-04-13 17:10:18 -0500

    moved files to root directory

---
+++ product/urls.py
@@ -0,0 +1,20 @@
+from . import views
+from .views import clear_comparison
+from django.urls import path
+from .views import report_product
+
+
+app_name = 'product'
+
+
+urlpatterns = [
+    path('get_product_name/<int:product_id>/', views.get_product_name, name='get_product_name'),
+    path('compare/clear/', clear_comparison, name='compare-clear'),
+    path('remove_compare/<int:product_id>/', views.remove_compare, name='remove_compare'),
+    path('compare/<int:product_id>/', views.compare, name='compare'),
+    path('search', views.search, name="search"),
+    path('product/report_product/<int:product_id>/', views.report_product, name='report_product'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product, name="product"),
+    path('<slug:category_slug>/', views.category, name="category"),
+
+]

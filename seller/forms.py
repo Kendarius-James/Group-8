812ff4a401c497717269114e31d4775ef08d80aa
Justin Whitfield <91854905+justinwhitfieldd@users.noreply.github.com>
2023-04-30 13:52:17 -0500

    added report seller

--- seller/forms.py
+++ seller/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm, models
-
+from django import forms
 from product.models import Product
+from .models import SellerReport
 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['category', 'image', 'title', 'description', 'price', 'quantity']
+
+class SellerReportForm(forms.ModelForm):
+    class Meta:
+        model = SellerReport
+        fields = ['reason']

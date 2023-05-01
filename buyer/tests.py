8a22454ba370259682525b11d9a8c5d7057c61d5
laurenfortnerr <laurenafortner@gmail.com>
2023-05-01 01:19:41 -0500

    test cases for admin

--- buyer/tests.py
+++ buyer/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+from django.contrib.auth.models import User
+from accounts.models import BuyerProfile
+
+class BuyerProfileAdminTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(email='test@example.com', password='password')
+        self.buyer_profile = BuyerProfile.objects.create(user=self.user)
+
+    def test_buyer_profile_list_display(self):
+        self.client.login(email='test@gmail.com', password='password')
+        response = self.client.get('/admin/buyerprofile/')
+        self.assertContains(response, self.user.email)
+        self.assertContains(response, self.user.password)
